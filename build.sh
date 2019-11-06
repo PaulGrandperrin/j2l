1785cb1d7f034bb7026570584ea03247f290340f
Paul Grandperrin <paul.grandperrin@gmail.com>
2019-11-06 15:28:18 +0100

    build.sh: fix

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 set -xe
 
-if ! hash cargo 2>/dev/null; then
-  if [ "`uname`" = "Linux" ] ;then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal -t x86_64-unknown-linux-musl
-  else
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal
+if [ "`uname`" = "Linux" ] ;then
+  if ! hash cargo 2>/dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal -t x86_64-unknown-linux-musl # MUSL is for static linking on Linux
+    . $HOME/.cargo/env
   fi
+
+  apt install -y --no-install-recommends build-essential musl-tools
+
+  cargo build --release --target x86_64-unknown-linux-musl
+  echo "the resulting binary is at ./target/x86_64-unknown-linux-musl/release/j2l"
+else
+  if ! hash cargo 2>/dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal
+    . $HOME/.cargo/env
+  fi
+
+  cargo build --release
+  echo "the resulting binary is at ./target/release/j2l"
 fi
 
-cargo build --release
-
-# the resulting binary is at ./target/release/j2l
